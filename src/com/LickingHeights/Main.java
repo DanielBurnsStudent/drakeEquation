1fbb26e2b1759e73c63909f978a3336ebee80f62
Daniel <daniel.burns.2022@student.lhschools.org>
2020-01-31 12:28:01 -0500

    Added numberOfExoplanets User Input

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -32,11 +32,14 @@ L = 1000 to 100,000,000 years (which will last somewhere between 1000 and 100,00
         Scanner keyboard;
         keyboard = new Scanner(System.in);
 
-        System.out.println("How many stars are formed every year in our galaxy?");
+        System.out.println("How many stars are formed every year in our galaxy? (1)");
         rStar = keyboard.nextInt();
 
         System.out.println("How many of these stars produce planets? (0.2 to 0.5)");
         f_p = keyboard.nextInt();
+
+        System.out.println("Stars with planets will have between 1 and 5 capable of life ( 1 to 5)");
+        numberOfExoplanets = keyboard.nextInt();
 
         N = rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L;
 
