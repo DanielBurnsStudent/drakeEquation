6b4913320cfb91a68cae3c68511d784bf72860f1
Daniel <daniel.burns.2022@student.lhschools.org>
2020-01-31 12:32:55 -0500

    Added L User Input

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -50,6 +50,9 @@ L = 1000 to 100,000,000 years (which will last somewhere between 1000 and 100,00
         System.out.println("What percent of these will be able to communicate? (0-100%)");
         f_c = keyboard.nextInt();
 
+        System.out.println("How long will these civilizations last? (1,000 to 1000,000,000 years)");
+        L = keyboard.nextInt();
+
         N = rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L;
 
         System.out.println("The number of intelligent species that are out there \n" +
